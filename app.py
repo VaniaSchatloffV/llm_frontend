2c71cc2424bfdc2dd4864d3424d6050dabe25900
VaniaSchatloffV <v.schatloff@gmail.com>
2024-07-07 17:38:17 -0400

    cambiando app

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint, jsonify
 from markupsafe import escape
 from flask import url_for
 from flask import request
 from flask import render_template
 
 app = Flask(__name__)
-
+bp = Blueprint('api', __name__, url_prefix='/ServiceDev/5000')
 
 @app.get('/')
 def index():
     return 'index'
 
+app.register_blueprint(bp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8002)
