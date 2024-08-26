3ce276e4c821cab135f8eeb26e4f635e5acf0312
Vania Schatloff <v.schatloff@gmail.com>
2024-08-26 17:05:04 -0400

    agregando sidebar

--- static/scripts/chat_script.js
+++ static/scripts/chat_script.js
@@ -53,5 +53,23 @@ function loadMessages() {
     });
 }
 
+function open_sidebar() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    //document.getElementById("openNav").style.display = 'none';
+    document.getElementById("openNav").style.visibility = 'hidden';
+    document.getElementById("openNav").style.pointerEvents = 'none';
+  }
+  
+  function close_sidebar() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    //document.getElementById("openNav").style.display = "inline-block";
+    document.getElementById("openNav").style.visibility = 'visible';
+    document.getElementById("openNav").style.pointerEvents = 'auto';
+
+  }
+
 // Cargar mensajes al cargar la página
 loadMessages();
