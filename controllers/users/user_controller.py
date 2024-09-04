631d6995788d6e547c8e759bf10519809f81145e
Vania Schatloff <v.schatloff@gmail.com>
2024-09-04 18:56:13 -0400

    quitando repositorios

--- controllers/users/user_controller.py
+++ controllers/users/user_controller.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, jsonify, url_for, request, render_template, redirect, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from handlers.DBHandler import DBHandler
-from repositories.users import user_repository
+from handlers.DBORMHandler import DB_ORM_Handler
+from models.users import UserObject
+from models.roles import RoleObject
 
 
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    user = user_repository.get_user(email)
+    user = get_user(email)
     if user != {} and check_password_hash(user.get("password"), password):
-        role = user_repository.get_role(user.get("role_id"))
+        role = get_role(user.get("role_id"))
         session['user_id'] = user.get("id")
         session['user_name'] = user.get("name")
         session['user_lastname'] = user.get("lastname")
@@ -40,11 +41,11 @@ def register():
     
     hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
     try:
-        existing_user = user_repository.get_user(email)
+        existing_user = get_user(email)
         if existing_user != {}:
             flash('El correo electrónico ya está registrado', 'danger')
             return redirect(url_for('users.register'))
-        user_repository.insert_new_user(email, name, lastname, hashed_password)
+        insert_new_user(email, name, lastname, hashed_password)
         flash('Registro exitoso. Por favor, inicia sesión', 'success')
         return redirect(url_for('users.index'))
     except Exception as e:
@@ -55,3 +56,49 @@ def logout():
     session.clear()
     flash('Sesión cerrada.', 'success')
     return redirect(url_for('users.index'))
+
+def get_user(user_email: str):
+    """
+    Retorna diccionario con
+        - id
+        - name
+        - lastname
+        - password
+        - role_id
+    Almacenados en la base de datos. Si el usuario no existe, retorna diccionario vacío.
+    """
+    with DB_ORM_Handler() as db:
+        user = db.getObjects(
+            UserObject,
+            UserObject.email == user_email,
+            defer_cols=[],
+            columns=[UserObject.id, UserObject.name, UserObject.lastname, UserObject.role_id, UserObject.password]
+        )
+        if len(user) == 0:
+            return {}
+        return user.pop()
+
+
+def get_role(role_id: int):
+    with DB_ORM_Handler() as db:
+        role = db.getObjects(
+            RoleObject,
+            RoleObject.id == role_id,
+            defer_cols=[],
+            columns=[RoleObject.role_name]
+        )
+        if len(role) == 0:
+            return None
+        return role.pop().get("role_name")
+
+def insert_new_user(email: str, name: str, lastname: str, password: str):
+    User = UserObject()
+    User.email = email
+    User.name = name
+    User.lastname = lastname
+    User.password = password
+    User.role_id = 2
+    with DB_ORM_Handler() as db:
+        db.createTable(User)
+        db.saveObject(User)
+        
